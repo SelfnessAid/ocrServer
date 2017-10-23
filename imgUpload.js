6b82483423fd0cbeeefe9ceaee7f07ed03efd66a
SelfnessAid <daxianjin59@hotmail.com>
2017-10-23 11:39:23 +0800

    simple server for GCS

---
+++ imgUpload.js
@@ -0,0 +1,46 @@
+'use strict';
+const storage = require('@google-cloud/storage');
+const fs = require('fs')
+
+const gcs = storage({
+  projectId: 'm3t3r-app',
+  keyFilename: './M3T3R-bbbccb888201.json'
+});
+
+const bucketName = 'm3t3r-pics'
+const bucket = gcs.bucket(bucketName);
+
+function getPublicUrl(filename) {
+  return 'https://storage.googleapis.com/' + bucketName + '/' + filename;
+}
+
+let ImgUpload = {};
+
+ImgUpload.uploadToGcs = (req, res, next) => {
+  if(!req.file) return next();
+
+  // Can optionally add a path to the gcsname below by concatenating it before the filename
+  const gcsname = req.file.originalname;
+  const file = bucket.file(gcsname);
+
+  const stream = file.createWriteStream({
+    metadata: {
+      contentType: req.file.mimetype
+    }
+  });
+
+  stream.on('error', (err) => {
+    req.file.cloudStorageError = err;
+    next(err);
+  });
+
+  stream.on('finish', () => {
+    req.file.cloudStorageObject = gcsname;
+    req.file.cloudStoragePublicUrl = getPublicUrl(gcsname);
+    next();
+  });
+
+  stream.end(req.file.buffer);
+}
+
+module.exports = ImgUpload;
